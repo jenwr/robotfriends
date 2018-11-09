0d1877cb37aabb51ef3ad5c18494d964989ac819
jenwr <jenwr@users.noreply.github.com>
2018-11-08 19:02:33 -0500

    Updates

---
+++ precache-manifest.8bbfddbfc93e6a107e7187afa2928f64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf9896a2b59954b711e1b5d5a63c2fbc",
+    "url": "/robotfriends/static/media/SEGA.cf9896a2.woff"
+  },
+  {
+    "revision": "036993615acbccafb03f",
+    "url": "/robotfriends/static/js/runtime~main.03699361.js"
+  },
+  {
+    "revision": "4ebcd6b14a9b42b2509a",
+    "url": "/robotfriends/static/js/main.4ebcd6b1.chunk.js"
+  },
+  {
+    "revision": "bc9009cd157c86927107",
+    "url": "/robotfriends/static/js/1.bc9009cd.chunk.js"
+  },
+  {
+    "revision": "4ebcd6b14a9b42b2509a",
+    "url": "/robotfriends/static/css/main.5e2d795a.chunk.css"
+  },
+  {
+    "revision": "bc9009cd157c86927107",
+    "url": "/robotfriends/static/css/1.162ffd45.chunk.css"
+  },
+  {
+    "revision": "d9015acee790a76f2674f2c8eb244d3e",
+    "url": "/robotfriends/index.html"
+  }
+];
